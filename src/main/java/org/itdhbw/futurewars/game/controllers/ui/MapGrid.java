7e23145aeb2691447249c11a4ae6c8f4c72fc402
Simon <simon21.blum@gmail.com>
2024-06-03 11:00:54 +0200

    Add MapGrid

---
+++ src/main/java/org/itdhbw/futurewars/game/controllers/ui/MapGrid.java
@@ -0,0 +1,9 @@
+package org.itdhbw.futurewars.game.controllers.ui;
+
+import javafx.scene.layout.GridPane;
+
+public class MapGrid extends GridPane {
+    public MapGrid() {
+        super();
+    }
+}
